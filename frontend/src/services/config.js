da5ca2d68c869cb63ab2733fd08971393a542346
Noel Alemaña <30jrnoel@gmail.com>
2024-11-11 23:34:04 +0800

    Update apiConfig

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,8 +1,15 @@
-const apiHost = process.env.REACT_APP_API_HOST ?? '127.0.0.1';
-const apiPort = process.env.REACT_APP_API_PORT ?? '8000';
+const isProduction = process.env.NODE_ENV === 'production';
+
+const apiHost = isProduction
+  ? process.env.REACT_APP_API_HOST // Production API host from environment
+  : '127.0.0.1';
+
+const apiPort = isProduction ? '' : '8000'; // No port in production if using default HTTP/HTTPS
 
 const apiConfig = {
-  API_URL: `http://${apiHost}:${apiPort}`,
+  API_URL: isProduction
+    ? `https://technosystem-backend.azurewebsites.net/` // Use HTTPS for production, without a port
+    : `http://${apiHost}:${apiPort}`, // Local URL for development
 };
 
 export default apiConfig;
